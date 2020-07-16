39c1c6f45f80aa608c96e7cceff343cc2962e76f
TeoReu <reu.teodora@gmail.com>
2020-07-16 09:12:43 +0000

    Improved gallery, added My favorite songs section in Blog, solved minor CSS problems.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -57,7 +57,42 @@ function search(){
     window.location.replace("https://github.com/TeoReu/teoreu.github.io/blob/master/Euler_s_Identity%20(8).pdf");
 }
 
-function changePhoto(){
-    const nr = Math.floor((Math.random() * 6) + 1);
-    document.getElementById("photo").src = "images/" + nr + ".png";
+var nr = 1
+
+function changePhotoRight(){
+    if (nr==6){
+        nr = 1;
+    } else {
+        nr = nr + 1;
+    }
+    document.getElementById("photo").src = "images/gallery/" + nr + ".png";
+}
+
+function changePhotoLeft(){
+    if (nr==1){
+        nr = 6;
+    } else {
+        nr = nr - 1;
+    }
+    document.getElementById("photo").src = "images/gallery/" + nr + ".png";
+}
+
+function getMusic(){
+    const songs = ["https://www.youtube.com/watch?v=-EzURpTF5c8",
+    "https://www.youtube.com/watch?v=TNRCvG9YtYI",
+    "https://www.youtube.com/watch?v=So718wk426c",
+    "https://www.youtube.com/watch?v=8B1oIXGX0Io",
+    "https://www.youtube.com/watch?v=d27gTrPPAyk",
+    "https://www.youtube.com/watch?v=hYKYka-PNt0",
+    "https://www.youtube.com/watch?v=SRmCEGHt-Qk",
+    "https://www.youtube.com/watch?v=bVeOdm-29pU",
+    "https://www.youtube.com/watch?v=RBSaCAxJMdI",
+    "https://www.youtube.com/watch?v=lXYKGL6MgKM",
+    "https://www.youtube.com/watch?v=u9sq3ME0JHQ",
+    "https://www.youtube.com/watch?v=3zrSoHgAAWo",
+    "https://www.youtube.com/watch?v=eKVQTLLUR3U",
+    "https://www.youtube.com/watch?v=-dD2hTNEL3g"];
+
+    const song = songs[Math.floor(Math.random() * songs.length)];
+     window.location.replace(song);
 }
