1ce6d1b80749bfb73147f4689653779111aaf2be
TeoReu <reu.teodora@gmail.com>
2020-07-27 09:18:34 +0000

    Updating TeoDevelopment branch

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -234,10 +234,10 @@ function setCookie(cname,cvalue,exdays) {
 function getCookie(cname) {
   var name = cname + "=";
   var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
+  var ca = decodedCookie.split(";");
   for(var i = 0; i < ca.length; i++) {
     var c = ca[i];
-    while (c.charAt(0) == ' ') {
+    while (c.charAt(0) == " ") {
       c = c.substring(1);
     }
     if (c.indexOf(name) == 0) {
