7c9def3d0c8a5b5d3eb7202b779cd73c9040453c
TeoReu <reu.teodora@gmail.com>
2020-07-28 17:17:53 +0000

    Google Maps API

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -276,6 +276,7 @@ function checkLoginStatus(){
   }
 }
 
+/*
 function fetchBlobstoreUrlAndShowForm() {
   fetch('/blobstore-upload-url')
     .then((response) => {
@@ -301,4 +302,17 @@ function createBlobElement(blob){
   blobElement.src = blob.url;
 
   return blobElement;
+}
+*/
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: 45.5351,
+      lng: 25.2181
+    },
+    zoom: 11
+  });
 }
