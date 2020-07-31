116f1bcc5dd50644178729828b380db17b04fa70
TeoReu <reu.teodora@gmail.com>
2020-07-31 08:47:58 +0000

    Fixes.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+var mykey = config.MY_KEY;
 /**
  * Adds a random greeting to the page.
  */
